80021dad139999705fa0d1a0c70087eb021f8943
raige <raigekka0128@yahoo.co.jp>
2020-06-16 12:45:41 +0900

    +更新　index.jsの修正 +更新　main.jsの修正

--- src/router/index.js
+++ src/router/index.js
@@ -2,21 +2,20 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 // components
-import UserLogin from '/components/UserLogin.vue'
+import UserLogin from '@/components/UserLogin.vue'
 
 Vue.use(Router)
 
-const router = new Router({
-  routes: [
-    {
-      path: '/login',
-      name: 'login',
-      component: UserLogin,
-      meta: {
-          isPublic: true
-      }
+export default new Router({
+      routes: [
+        {
+          path: '/login',
+          name: 'login',
+          component: UserLogin,
+          meta: {
+              isPublic: true
+          }
+        }
+      ]
     }
-  ]
-})
-
-export default router
+)
