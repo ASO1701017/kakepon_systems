840531a7cf9298bd600873a9f00281236bf0eea3
ASO1701019 <1701019@st.asojuku.ac.jp>
2020-06-19 11:32:12 +0900

    front develop add Save_calorie.vue update router index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,25 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+//importするコンポーネント
+import Top from "../components/Top";
+import Save_calorie from "../components/Save_calorie";
+
 Vue.use(Router)
 
 export default new Router({
     routes:[
+        // {
+        //     path: '/sample', <-パス
+        //     component: sample <-コンポーネント
+        // },
         {
-            path:'/',
-            //component: 初期画面のファイル名
+            path: '/Top',
+            component: Top
         },
+        {
+            path: '/Save_calorie',
+            component: Save_calorie,
+        }
     ]
 })
