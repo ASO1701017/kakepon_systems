af7d3591dd764af17c4234bd970d8568b1e65b27
raige <raigekka0128@yahoo.co.jp>
2020-06-19 12:11:41 +0900

    +update App.vueに背景を追加 +update UserLogin.vueにデザインの追加

--- src/components/UserLogin.vue
+++ src/components/UserLogin.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="#app">
         <section>
-            <div>
+            <div id="login-back">
                 <div id="title-login">
                     acsys にログイン
                 </div>
@@ -12,13 +12,13 @@
                     <label for="Mail"><input type="email" id="Mail" placeholder="メールアドレス" v-model="MailAddress"></label>
                 </div>
                 <div>
-                    <label for="Pass"><input type="Password" id="Pass" placeholder="パスワード" v-model="Password"></label>
+                    <label for="Pass"><input type="Password" id="Pass" placeholder="パスワード" v-model="Password" ></label>
                 </div>
-                <button @click="login" :disabled="!isValidated">ログイン</button>
+                <button @click="login"  class="btn-flat-vertical-border">ログイン</button>
                 <UserLogin />
+            <div id="new-account" >
+                <router-link to="/signin" class="cp_link">アカウントの新規作成</router-link>
             </div>
-            <div id="new-account">
-                <router-link to="/signin">アカウントの新規作成</router-link>
             </div>
         </section>
     </div>
@@ -31,16 +31,9 @@
     const auth = {
         login:function (mail,pass) {
             window.alert("mailaddress:" + mail + "\n" + "password:" + pass)
-            if (mail === "こんばんわ"){
-                window.alert("成功")
-                this.$router.replace("/")
-            }else {
-                this.ErrorMessage = false
-            }
+            return 2
         }
     };
-
-
 
     export default {
         components:{
@@ -56,13 +49,13 @@
         methods:{
             login:function () {
                 // eslint-disable-next-line no-unused-vars
-                this.ErrorMessage = auth.login(this.MailAddress, this.Password);
+                const check = auth.login(this.MailAddress, this.Password);
+                if (check === 1){
+                    this.$router.replace("/")
+                }else {
+                    this.ErrorMessage = false
+                }
             },
-        },
-        computed: {
-            isValidated() {
-                return this.MailAddress !== "" && this.Password !== ""
-            }
         },
     }</script>
 
@@ -73,16 +66,33 @@
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
         text-align: center;
+        margin: 10px;
     }
 
     #title-login{
         font-size: 40px;
-        margin-top: 100px;
+        margin-bottom: 10px;
     }
 
     #new-account{
         font-size: 20px;
         margin-top: 15px;
+    }
+
+    #login-back{
+        margin-top: 100px;
+        /*background: white;*/
+        /*display: inline-block;*/
+    }
+
+    #error-login{
+        font-size: 25px;
+        margin-top: 20px;
+        color: #ff7d6e;
+        background: #ffebe9;
+        border-top: solid 10px #ff7d6e;
+        padding: 0.5em 1em;
+        display: inline-block;
     }
 
     input {
@@ -91,6 +101,13 @@
         margin-top: 40px;
     }
 
+    input:focus{
+        border: 2px solid #1e4000;
+        z-index: 10;
+        outline: 0;
+    }
+
+
     button {
         width: 25em;
         font-size: 20px;
@@ -98,6 +115,38 @@
         padding:5px;
         line-height: normal;
         display:inline-block;
+    }
+
+    .btn-flat-vertical-border {
+      position: relative;
+      display: inline-block;
+      font-weight: bold;
+      padding: 0.5em 1em;
+      text-decoration: none;
+      border-left: solid 4px #6cbb00;
+      border-right: solid 4px #6cbb00;
+      color: #6cbb00;
+      background: #fafff6;
+      transition: .4s;
+    }
+
+    .btn-flat-vertical-border:hover {
+      background: #6cbb00;
+      color: #FFF;
+    }
+
+    .cp_link {
+        padding: 0.1em 0.3em;
+        background-image: linear-gradient(to right, rgba(0, 0, 0, 0) 50%, rgb(131, 144, 7) 50%);
+        background-position: 0 0;
+        background-size: 200% auto;
+        transition: .3s;
+        color: #016500;
+        text-decoration: none;
+    }
+    .cp_link:hover {
+        background-position: -100% 0;
+        color: #fff;
     }
 
     @media only screen and (max-device-width : 850px) {
