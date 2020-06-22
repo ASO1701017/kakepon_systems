407dbb0bfa890b7e2d55a7a0ce5161c22ed4c37e
raige <raigekka0128@yahoo.co.jp>
2020-06-23 04:39:48 +0900

    +update UserSignIn.vueのフォーム作成

--- src/components/UserSignIn.vue
+++ src/components/UserSignIn.vue
@@ -1,11 +1,193 @@
-
+<template>
+    <div id="#app">
+        <UserSignIn />
+        <section>
+            <div id="signin-backstyle">
+                <div id="signin-title">
+                    acsys に新規登録
+                </div>
+                <div id="error-signin" v-if="!SignInErrorMessage">
+                    入力情報が間違ってる項目があります
+                </div>
+                <div class="signin-information-title">
+                    個人情報
+                </div>
+                <div>
+                    <label for="Name" >名前</label><br>
+                    <input type="text" id="Name" v-model="UserName">
+                </div>
+                <div>
+                    <label for="Year">年</label><br>
+                    <input type="text" id="Year" v-model="UserBirthdayYear">
+                </div>
+                <div>
+                    <label for="Month">月</label><br>
+                    <input type="text" id="Month" v-model="UserBirthdayMonth">
+                </div>
+                <div>
+                    <label for="Day">日</label><br>
+                    <input type="text" id="Day" v-model="UserBirthdayDay">
+                </div>
+                <div>
+                    <label for="Gender">性別</label><br>
+                    <input type="radio" name="Gender" id="Gender" value="男性" v-model="UserGender">男性
+                    <input type="radio" name="Gender" value="女性" v-model="UserGender">女性
+                </div>
+                <div class="signin-information-title">
+                    アカウント情報
+                </div>
+                <div>
+                    <label for="MailAddress">メールアドレス</label><br>
+                    <input type="email" id="MailAddress" v-model="UserMail">
+                </div>
+                <div>
+                    <label for="Password">パスワード</label><br>
+                    <input type="password" id="Password" v-model="UserPass">
+                </div>
+                 <div class="signin-information-title">
+                    身体情報
+                </div>
+                <div>
+                    <label for="Height">身長</label><br>
+                    <input type="text" id="Height" v-model="UserHeight">
+                </div>
+                <div>
+                    <label for="BodyWeight">体重</label><br>
+                    <input type="text" id="BodyWeight" v-model="UserBodyWeight">
+                </div>
+                <div>
+                    <label>身体活動レベル</label>
+                        <select name=”ActivLebel” v-model="UserActiveLevel">
+                        <option value=”one”>レベルⅠ</option>
+                        <option value=”two”>レベルⅡ</option>
+                        <option value=”three”>レベルⅢ</option>
+                        </select>
+                    <div>
+                        レベルⅠ	→	生活の大部分が座位で、静的な活動が中心の場合
+                        レベルⅡ	→	座位中心の仕事だが、職場内での移動や立位での作業・接客等、あるいは通勤・買物・家事、軽いスポーツ等のいずれかを含む場合
+                        レベルⅢ	→ 移動や立位の多い仕事への従事者。あるいは、スポーツなど余暇における活発な運動習慣をもっている場合
+                    </div>
+                </div>
+            <button @click="signin"  class="btn-flat-vertical-border">登録</button>
+            </div>
+        </section>
+    </div>
+</template>
 
 <script>
+
+    import UserSignIn from './UserSignIn.vue'
+
     export default {
-        name: "UserSignIn"
+        components:{
+            UserSignIn
+        },
+        data(){
+            return{
+                SignInErrorMessage:true,
+                UserName:"",
+                UserBirthdayYear:"",
+                UserBirthdayMonth:"",
+                UserBirthdayDay:"",
+                UserGender:"",
+                UserMail:"",
+                UserPass:"",
+                UserHeight:"",
+                UserBodyWeight:"",
+                UserActiveLevel:"",
+            }
+        },
+        methods:{
+            signin:function () {
+
+            }
+        }
     }
 </script>
 
 <style scoped>
+
+        /*ボタン、テキスト、divの設定*/
+    button,input,div{
+        /*フォント設定*/
+        font-family: "Montserrat","游ゴシック",YuGothic,"ヒラギノ角ゴ ProN W3","Hiragino Kaku Gothic ProN","メイリオ",Meiryo,sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #5c905c;
+    }
+
+    button,div{
+        margin: 10px 30px;
+    }
+
+    label{
+        float: left;
+        margin-top: 10px;
+        font-size: 17px;
+    }
+
+    /*背景を白に*/
+    #signin-backstyle{
+        margin-top: 60px;
+        background: white;
+        display: inline-block;
+    }
+
+    /*acsysにログインのスタイル*/
+    #signin-title{
+        margin-top: 30px;
+        font-size: 40px;
+        font-weight: bolder;
+    }
+
+        /*inputのスタイル*/
+    input {
+        /*width: 25em;*/
+        font-size: 20px;
+    }
+
+    /*inputフォーカス時*/
+    input:focus{
+        border: 2px solid #5c905c;
+        z-index: 10;
+        outline: 0;
+    }
+
+    .signin-information-title{
+        border-bottom: 2px solid #5c905c;
+        text-align: left;
+        color: #5c905c;
+        font-size: 20px;
+        margin-top: 20px;
+    }
+
+        /*buttonの設定*/
+    button {
+        width: 25em;
+        font-size: 20px;
+        margin-top: 30px;
+        padding:5px;
+        line-height: normal;
+        display:inline-block;
+    }
+
+    .btn-flat-vertical-border {
+      position: relative;
+      display: inline-block;
+      font-weight: bold;
+      padding: 0.5em 1em;
+      text-decoration: none;
+      border-left: solid 4px #FF839007;
+      border-right: solid 4px #FF839007;
+      color: #ffffff;
+      background: #5c905c;
+      transition: .4s;
+    }
+
+    .btn-flat-vertical-border:hover {
+      background: #283d28;
+      color: #FFF;
+    }
 
 </style>
