4e5bdff8c76114b3b269817df92a4bc262a08a60
raige <raigekka0128@yahoo.co.jp>
2020-06-22 11:51:32 +0900

    +update デザインの調整

--- src/components/UserLogin.vue
+++ src/components/UserLogin.vue
@@ -1,8 +1,8 @@
 <template>
     <div id="#app">
         <section>
-            <div id="login-back">
-                <div id="title-login">
+            <div id="login-backstyle">
+                <div id="login-title">
                     acsys にログイン
                 </div>
                 <div id="error-login" v-if="!ErrorMessage">
@@ -16,7 +16,7 @@
                 </div>
                 <button @click="login"  class="btn-flat-vertical-border">ログイン</button>
                 <UserLogin />
-            <div id="new-account" >
+            <div id="login-new-account" >
                 <router-link to="/signin" class="cp_link">アカウントの新規作成</router-link>
             </div>
             </div>
@@ -31,6 +31,8 @@
     const auth = {
         login:function (mail,pass) {
             window.alert("mailaddress:" + mail + "\n" + "password:" + pass)
+            //ここでAPIに送信
+            //結果をリターンする
             return 2
         }
     };
@@ -61,33 +63,55 @@
 
 <style scoped>
 
-    button, input,div{
+    /*ボタン、テキスト、divの設定*/
+    button,input,div{
+        /*フォント設定*/
         font-family: "Montserrat","游ゴシック",YuGothic,"ヒラギノ角ゴ ProN W3","Hiragino Kaku Gothic ProN","メイリオ",Meiryo,sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
         text-align: center;
-        margin: 10px;
+        margin: 10px 30px;
     }
 
-    #title-login{
+    /*背景を白に*/
+    #login-backstyle{
+        margin-top: 60px;
+        background: white;
+        display: inline-block;
+    }
+
+    /*acsysにログインのスタイル*/
+    #login-title{
+        margin-top: 30px;
         font-size: 40px;
-        margin-bottom: 10px;
+        color: #5c905c;
+        font-weight: bolder;
     }
 
-    #new-account{
+    /*新規作成のリンク*/
+    #login-new-account{
         font-size: 20px;
         margin-top: 15px;
     }
 
-    #login-back{
-        margin-top: 100px;
-        /*background: white;*/
-        /*display: inline-block;*/
+    .cp_link {
+        padding: 0.1em 0.3em;
+        background-image: linear-gradient(to right, rgba(0, 0, 0, 0) 50%, rgb(92, 144, 92) 50%);
+        background-position: 0 0;
+        background-size: 200% auto;
+        transition: .3s;
+        color: #5c905c;
+        text-decoration: none;
+    }
+    .cp_link:hover {
+        background-position: -100% 0;
+        color: #fff;
     }
 
+    /*エラー文の表示エリア*/
     #error-login{
-        font-size: 25px;
-        margin-top: 20px;
+        font-size: 24px;
+        margin-top: 15px;
         color: #ff7d6e;
         background: #ffebe9;
         border-top: solid 10px #ff7d6e;
@@ -95,23 +119,25 @@
         display: inline-block;
     }
 
+    /*inputのスタイル*/
     input {
         width: 25em;
         font-size: 20px;
-        margin-top: 40px;
+        margin-top: 30px;
     }
 
+    /*inputフォーカス時*/
     input:focus{
-        border: 2px solid #1e4000;
+        border: 2px solid #5c905c;
         z-index: 10;
         outline: 0;
     }
 
-
+    /*buttonの設定*/
     button {
         width: 25em;
         font-size: 20px;
-        margin-top: 40px;
+        margin-top: 30px;
         padding:5px;
         line-height: normal;
         display:inline-block;
@@ -123,30 +149,16 @@
       font-weight: bold;
       padding: 0.5em 1em;
       text-decoration: none;
-      border-left: solid 4px #6cbb00;
-      border-right: solid 4px #6cbb00;
-      color: #6cbb00;
-      background: #fafff6;
+      border-left: solid 4px #FF839007;
+      border-right: solid 4px #FF839007;
+      color: #ffffff;
+      background: #5c905c;
       transition: .4s;
     }
 
     .btn-flat-vertical-border:hover {
-      background: #6cbb00;
+      background: #283d28;
       color: #FFF;
-    }
-
-    .cp_link {
-        padding: 0.1em 0.3em;
-        background-image: linear-gradient(to right, rgba(0, 0, 0, 0) 50%, rgb(131, 144, 7) 50%);
-        background-position: 0 0;
-        background-size: 200% auto;
-        transition: .3s;
-        color: #016500;
-        text-decoration: none;
-    }
-    .cp_link:hover {
-        background-position: -100% 0;
-        color: #fff;
     }
 
     @media only screen and (max-device-width : 850px) {
