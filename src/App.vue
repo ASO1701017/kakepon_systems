c142ced55e89927f79843adb802b5ca67546a939
raige <raigekka0128@yahoo.co.jp>
2020-06-24 03:22:23 +0900

    +update Headerの作成　ログインと新規作成のみ

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,105 @@
 <template>
-  <div>
-<!--   <UserLogin />-->
+  <div id="#app">
+    <header>
+        <div id="HeaderBackGroundColor">
+            <input type="button" @click="StartLogin" value="ログイン" id="HeaderLoginButton" class="btn-flat-vertical-border-login">
+            <input type="button" @click="StartSignIn" value="新規登録" id="HeaderSignInButton" class="btn-flat-vertical-border-signin">
+        </div>
+    </header>
       <router-view></router-view>
   </div>
+
 </template>
 
 <script>
-// import UserLogin from './components/UserLogin.vue'
 
 export default {
   name: 'App',
-  components: {
-    // UserLogin
+  data(){
+      return{
+          UserToken:""
+      }
+  },
+  methods:{
+      StartLogin:function () {
+        this.$router.push("/login")
+      },
+      StartSignIn:function () {
+        this.$router.push("/signin")
+      }
   }
 }
 </script>
 
 <style>
+
     html {
       background: #E6FFE9;
+      color: #5c905c;
     }
+
 </style>
 
+<style scoped>
+
+    #HeaderBackGroundColor{
+        background-color: white;
+        background-size: contain;
+        margin-top: -10px;
+        margin-left: -6px;
+        margin-right: -6px;
+    }
+
+    input{
+        margin-top: 10px;
+        margin-bottom: 10px;
+        text-align: right;
+        position: relative;
+        padding: 0.8em;
+    }
+
+    #HeaderLoginButton{
+        left: 82%;
+    }
+
+    #HeaderSignInButton{
+        left: 84%;
+    }
+
+    .btn-flat-vertical-border-login {
+      position: relative;
+      display: inline-block;
+      font-weight: bold;
+      padding: 0.5em 1em;
+      text-decoration: none;
+      border-left: solid 4px #FF839007;
+      border-right: solid 4px #FF839007;
+      color: #ffffff;
+      background: #5c905c;
+      transition: .4s;
+    }
+
+    .btn-flat-vertical-border-login:hover {
+      background: #283d28;
+      color: #FFF;
+    }
+
+    .btn-flat-vertical-border-signin {
+      position: relative;
+      display: inline-block;
+      font-weight: bold;
+      padding: 0.5em 1em;
+      text-decoration: none;
+      border-left: solid 4px #FF839007;
+      border-right: solid 4px #FF839007;
+      color: #ffffff;
+      background: #a77348;
+      transition: .4s;
+    }
+
+    .btn-flat-vertical-border-signin:hover {
+      background: #493100;
+      color: #FFF;
+    }
+
+</style>
